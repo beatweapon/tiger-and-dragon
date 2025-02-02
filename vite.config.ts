03bf0dcabdfbe6595a29a3d5dd198ec4ab2d1ab5
Edasan <beatweapon7th@gmail.com>
2025-02-03 05:40:41 +0900

    スマホでのデバッグのためにIPアドレスから接続できる設定に変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0', // 全てのネットワークインターフェースからアクセスを許可
+		port: 5173,
+	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
